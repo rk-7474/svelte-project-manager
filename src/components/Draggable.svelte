eb198d0fe427371734eae8d382c1e99cef574463
rk7474 <raggio271@gmail.com>
2023-09-10 20:06:52 +0200

    new file:   src/components/Draggable.svelte

---
+++ src/components/Draggable.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let top = 100;
+	
+	let moving = false;
+	
+	function onMouseDown() {
+		moving = true;
+	}
+	
+	function onMouseMove(e) {
+		if (moving) {
+			top += e.movementY;
+		}
+	}
+	
+	function onMouseUp() {
+		moving = false;
+	}
+	
+// 	$: console.log(moving);
+</script>
+
+<style>
+	.draggable {
+		user-select: none;
+		cursor: move;
+		border: solid 1px gray;
+		position: absolute;
+	}
+</style>
+
+<section on:mousedown={onMouseDown} style:top="{top}px" class="draggable">
+	<slot></slot>
+</section>
+
+<svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
